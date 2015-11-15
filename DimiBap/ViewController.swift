67f8b346ef356f1e4734922ffd1b98f3e1c56d64
sookcha <me@sookcha.com>
2015-11-16 08:40:36 +0900

    Initial Commit

---
+++ DimiBap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DimiBap
+//
+//  Created by Hoseong Son on 2015. 11. 16..
+//  Copyright © 2015년 Hoseong Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
